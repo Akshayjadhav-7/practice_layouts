215a6c1f605b197d10168fe914fbcb95b41dfa95
Akshay <akshay>
2021-04-29 13:37:15 +0530

    second change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
         body: SafeArea(
           child: Center(
             child: Column(
